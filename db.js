62604a5d69492c7b45d74606c9aceb3c97043463
张航 <672683505@qq.com>
2022-07-28 18:13:16 +0800

    init

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const { Sequelize, DataTypes } = require("sequelize");
 
 // 从环境变量中读取数据库配置
-const { MYSQL_USERNAME, MYSQL_PASSWORD, MYSQL_ADDRESS = "" } = process.env;
+const { MYSQL_USERNAME = "root", MYSQL_PASSWORD = "_Zz2134330", MYSQL_ADDRESS = "localhost:3306" } = process.env;
 
 const [host, port] = MYSQL_ADDRESS.split(":");
-
-const sequelize = new Sequelize("nodejs_demo", MYSQL_USERNAME, MYSQL_PASSWORD, {
+console.log(MYSQL_PASSWORD, ",", MYSQL_USERNAME, ',', MYSQL_ADDRESS)
+const sequelize = new Sequelize("node_admin", MYSQL_USERNAME, MYSQL_PASSWORD, {
   host,
   port,
   dialect: "mysql" /* one of 'mysql' | 'mariadb' | 'postgres' | 'mssql' */,
